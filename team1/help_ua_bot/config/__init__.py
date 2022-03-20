6aead610a83da509ff1d52f865ea5a4ee689dc8e
PaulinaPacyna <pacyna1998@gmail.com>
2022-03-20 08:56:57 +0100

    requests

--- team1/help_ua_bot/config/__init__.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "<key>"
-DIRECT_LINE_SECRET = "<key>"
-ADMIN_USER_NAMES = ["deeplearninggod"]
-QNA_MAKER_ENDPOINT = "https://ukraine-lang.cognitiveservices.azure.com/"
-QNA_SUBSCRIPTION_KEY = "<key>"
-ENDPOINT = "Test"
