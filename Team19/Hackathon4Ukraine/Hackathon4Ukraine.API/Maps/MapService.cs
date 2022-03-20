7f7403fb8281b42b9516f2e17202793f057c3559
Team19 <mhw64470@wp.pl>
2022-03-20 08:34:33 +0100

    Add app code

---
+++ Team19/Hackathon4Ukraine/Hackathon4Ukraine.API/Maps/MapService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hackathon4Ukraine.API.Maps
+{
+    public class MapService
+    {
+        public List<MapItem> Get()
+        {
+            return new FakeDataGenerator().GetMapItems();
+        }
+
+    }
+}
